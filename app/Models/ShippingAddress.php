b236104fb55c6622999601bea2ff01d8e77b8e79
herdifariz <fariz.herdi21@gmail.com>
2025-03-06 22:30:42 +0700

    add models and migrations

---
+++ app/Models/ShippingAddress.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ShippingAddress extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'order_id',
+        'recipient_name',
+        'address',
+        'city',
+        'postal_code',
+        'phone'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
---
+++ database/migrations/2025_03_06_141313_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->string('transaction_id');
+            $table->enum('payment_method', ['bank_transfer', 'credit_card', 'cod']);
+            $table->enum('payment_status', ['pending', 'completed', 'failed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
